7da47a1574b6579e56f139753a9163230a4b91ef
jgomesh <joao.gomeshugill@gmail.com>
2023-02-27 23:31:25 -0300

    styles: styling on /my_products path

--- frontend/src/pages/SellerProducts.tsx
+++ frontend/src/pages/SellerProducts.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import IProps from '../interfaces/IProps';
 import deleteItem from '../utils/api/deleteItem';
 import getSellerProducts from '../utils/api/getSellerProducts';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function SellerProducts(props: IProps) {
@@ -12,29 +13,35 @@ function SellerProducts(props: IProps) {
   const roleSeller = (userData.role === 'seller' || userData.role === 'admin');
   return (
     <>
-      {loading ? "Loading..." : (
-        <section>
-          <Header hiddeCart={hiddeCart} setHiddeCart={setHiddeCart} name={userData.name} history={props.history} roleUser={roleUser} roleSeller={roleSeller} />
-          {sellerProducts.map((product: any, index: number) => (
-            <div key={index}>
-              <h4>{product.name}</h4>
-              <img src={product.url_image} alt={product.name} />
-              <p>price {product.price}</p>
-              {roleSeller && (
-                <button onClick={async () => {
-                  setLoading(true);
-                  await deleteItem('product', product.id);
-                  const data = await getSellerProducts('my_products', product.seller_id);
-                  setSellerProducts(data.products);
-                  setLoading(false);
-                }}>
-                  DELETE PRODUCT
-                </button>
-              )}
-            </div>
-          ))}
-        </section>
-      )}
+      <Header hiddeCart={hiddeCart} setHiddeCart={setHiddeCart} name={userData.name} history={props.history} roleUser={roleUser} roleSeller={roleSeller} />
+          <section className='own_products_section'>
+            {loading ? "Loading..." : (
+              <>
+                {sellerProducts.map((product: any, index: number) => (
+                  <div className='own_products_div' key={index}>
+                    <h4>{product.name}</h4>
+                    <img className='seller_products' src={product.url_image} alt={product.name} />
+                    <p>Price: R$ {product.price}</p>
+                    {roleSeller && (
+                      <button
+                        className='delete_button'
+                        onClick={async () => {
+                          setLoading(true);
+                          await deleteItem('product', product.id);
+                          const data = await getSellerProducts('my_products', product.seller_id);
+                          setSellerProducts(data.products);
+                          setLoading(false);
+                        }}
+                      > 
+                        DELETE PRODUCT
+                      </button>
+                    )}
+                  </div>
+                ))}
+              </>
+            )}
+          </section>
+      <Footer setLoginOpen={() => {}} />
     </>
   );
 }
