705189a1d6128154f7fcaaeca0044e48c5bf2ebc
jgomesh <joao.gomeshugill@gmail.com>
2023-03-03 19:08:55 -0300

    feat: adding recharts framework in /dashboard route in frotend

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -6,6 +6,18 @@ import Footer from '../components/Footer';
 import Loading from '../components/Loading';
 import getTopSellers from '../utils/api/getTopSellers';
 import getTopProducts from '../utils/api/getTopProducts';
+import { PieChart, Pie, Tooltip, Legend } from 'recharts';
+
+function generateRandomHex() {
+  const letters = '0123456789ABCDEF';
+  let hex = '#';
+
+  for (let i = 0; i < 6; i++) {
+    hex += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return hex;
+}
 
 function Dashboard(props: IProps) {
   const [hiddeCart, setHiddeCart] = useState(true);
@@ -19,9 +31,24 @@ function Dashboard(props: IProps) {
     setLoading(true);
     const getSellers = async () => {
       const sellers = await getTopSellers();
+      const sellerUpdate = sellers.map((seller: any) => {
+        return ({
+          name: seller.name,
+          value: Number(seller.total_sales),
+          fill: generateRandomHex(),
+        })
+      })
       const products = await getTopProducts();
-      setTopProducts(products);
-      setTopSellers(sellers)
+
+      const productsUpdate = products.map((product: any) => {
+        return ({
+          name: product.name,
+          value: Number(product.total_sales),
+          fill: generateRandomHex(),
+        })
+      })
+      setTopProducts( productsUpdate);
+      setTopSellers(sellerUpdate)
       setLoading(false);
     };
     getSellers();
@@ -39,25 +66,53 @@ function Dashboard(props: IProps) {
             <div className='dashboard_section'>
               <div className='dashboard_div'>
                 <h3>Vendedores com mais vendas:</h3>
+                <PieChart width={400} height={400}>
+                  <Pie
+                    data={topSellers}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                  />
+                  <Tooltip />
+                  <Legend />
+                </PieChart>
                 <div className='top_sellers'>
                   {topSellers.map((seller: any, index: number) => {
                     return (
                     <div key={`seller-${index}`}>
                       <span>{seller.name}</span>
-                      <h6>Vendas: {seller.total_sales}</h6>
+                      <span className='font-12'>Vendas: {seller.value}</span>
                     </div>
                     )
                   })}
                 </div>
               </div>
-              <div className='dashboard_div'>
+              <div className='dashboard_div'  >
                 <h3>Produtos com mais vendas:</h3>
-                <div className='top_products'>
+                <PieChart width={400} height={400}>
+                  <Pie
+                    data={topProducts}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                  />
+                  <Tooltip />
+                  <Legend />
+                </PieChart>
+                <div className='top_sellers'>
                   {topProducts.map((seller: any, index: number) => {
                     return (
                     <div key={`seller-${index}`}>
                       <span>{seller.name}</span>
-                      <h6>Vendas: {seller.total_sales}</h6>
+                      <span className='font-12'>Vendas: {seller.value}</span>
                     </div>
                     )
                   })}
